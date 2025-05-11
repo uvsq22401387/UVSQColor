669cd1747a814e461b50150a3afefaa3e86f62f0
Anis TOUAT <anis.touat@ens.uvsq.fr>
2025-05-11 19:38:53 +0200

    apercu ajouté

--- Projet UVSQColor.py
+++ Projet UVSQColor.py
@@ -7,64 +7,67 @@ from scipy.signal import convolve2d
 
 fenetre_principale = None
 canvas = None
-image_originale = None
-image_affichee = None
-matrice_pixels = None
-matrice_affichage = None
+image_selectionnee = None 
+image_matrice = None 
+image_matrice_apercu = None
+image_affichee = None 
 dialogue_effet = None
+formats = "*.png;*.jpg;*.jpeg;*.bmp;*.gif"
 historique = []
 
 def ouvrir():
-    global image_originale, matrice_pixels
+    global image_selectionnee
+    global image_matrice
 
-    fichier = filedialog.askopenfilename(filetypes=[("Images", "*.png;*.jpg;*.jpeg;*.bmp;*.gif")])
+    fichier = filedialog.askopenfilename(filetypes=[("Images", formats)])
     if not fichier:
         return
-    image_originale = Image.open(fichier).convert("RGB")
-    largeur, hauteur = image_originale.size
-    matrice_pixels = [[image_originale.getpixel((x, y)) for x in range(largeur)]for y in range(hauteur)]
+    image_selectionnee = Image.open(fichier).convert("RGB")
+    largeur, hauteur = image_selectionnee.size
+    image_matrice = [[image_selectionnee.getpixel((x, y)) for x in range(largeur)]for y in range(hauteur)]
     refresh()
     activer_boutons()
 
 def activer_boutons():
-
-    menu_effets.entryconfig("Filtre vert", state="normal")
-    menu_effets.entryconfig("Niveaux de gris", state="normal")
+    sous_menu_filtres.entryconfig("Filtre vert", state="normal")
+    sous_menu_filtres.entryconfig("Niveaux de gris", state="normal")
+    sous_menu_ajustements.entryconfig("Luminosité", state="normal")
+    sous_menu_ajustements.entryconfig("Contraste", state="normal")
+    sous_menu_flou.entryconfig("Flou gaussien", state="normal")
     menu_fichier.entryconfig("Enregistrer", state="normal")
-    menu_effets.entryconfig("Luminosité", state="normal")
-    menu_effets.entryconfig("Contraste", state="normal")
-    menu_effets.entryconfig("Flou gaussien", state="normal")
-    menu_effets.entryconfig("Annuler", state="normal")
 
 def refresh():
-    global image_affichee, matrice_pixels
+    global image_affichee
 
-    hauteur = len(matrice_pixels)
-    largeur = len(matrice_pixels[0])
-    image_modifiee = Image.new("RGB", (largeur, hauteur))
+    hauteur = len(image_matrice)
+    largeur = len(image_matrice[0])
+    image = Image.new("RGB", (largeur, hauteur))
     for y in range(hauteur):
             for x in range(largeur):
-                couleur = matrice_pixels[y][x]
-                image_modifiee.putpixel((x, y), couleur)
-    image_affichee = ImageTk.PhotoImage(image_modifiee)
+                pixel = image_matrice[y][x]
+                image.putpixel((x, y), pixel)
 
+    image_affichee = ImageTk.PhotoImage(image)
     canvas.config(width=largeur, height=hauteur)
     canvas.create_image(0, 0, anchor=tk.NW, image=image_affichee)
-    fenetre_principale.pack_propagate(False)
 
 def sauvegarder_etat():
-    global historique, matrice_pixels
-    copie = [row[:] for row in matrice_pixels]
+    global historique
+    copie = [row[:] for row in image_matrice]
     historique.append(copie)
 
 def undo():
-    global historique, matrice_pixels
+    global historique
+    global image_matrice
     if historique:
-        matrice_pixels = historique.pop()
+        image_matrice = historique.pop()
         refresh()
 
 def fenetre_effet(fenetre=""):
-    global dialogue_effet, matrice_affichage
+    global dialogue_effet
+    global image_matrice_apercu
+
+    image_matrice_apercu = [ligne[:] for ligne in image_matrice]
 
     dialogue_effet = tk.Toplevel(fenetre_principale)
     frame_slider = tk.Frame(dialogue_effet)
@@ -72,25 +75,26 @@ def fenetre_effet(fenetre=""):
     frame_boutons = tk.Frame(dialogue_effet)
     frame_boutons.pack(side=tk.BOTTOM, pady=10)
     dialogue_effet.grab_set()
-    matrice_affichage = [row[:] for row in matrice_pixels]
 
-    if fenetre=="luminosité":     
+    if fenetre==0:     
         dialogue_effet.title("Luminosité")
         slider = tk.Scale(frame_slider, from_=0.1, to=3.0, orient=tk.HORIZONTAL, resolution=0.1, digits=2)
         slider.set(1.0)
         slider.pack(pady=20)
+        tk.Button(frame_boutons, text="Aperçu", command=lambda: apercu(slider.get(),None, 0)).pack(side=tk.LEFT, padx=10)
         tk.Button(frame_boutons, text="Appliquer", command=lambda: correction_gamma(slider.get())).pack(side=tk.LEFT, padx=10)
         tk.Button(frame_boutons, text="Annuler", command=dialogue_effet.destroy).pack(side=tk.LEFT, padx=10)
 
-    elif fenetre=="flou":
+    elif fenetre==1:
         dialogue_effet.title("Flou Gaussien")
         slider = tk.Scale(frame_slider, from_=1, to=10, orient=tk.HORIZONTAL, resolution=1, digits=0, label="Intensité du flou Gaussien")
         slider.set(1)
         slider.pack(pady=20)
+        tk.Button(frame_boutons, text="Aperçu", command=lambda: apercu(slider.get(),None, 1)).pack(side=tk.LEFT, padx=10)
         tk.Button(frame_boutons, text="Appliquer", command=lambda: appliquer_flou_gaussien(slider.get())).pack(side=tk.LEFT, padx=10)
         tk.Button(frame_boutons, text="Annuler", command=dialogue_effet.destroy).pack(side=tk.LEFT, padx=10)
 
-    elif fenetre == "contraste":
+    elif fenetre == 2:
         dialogue_effet.title("Contraste")
 
         slider_contraste = tk.Scale(frame_slider, from_=0.1, to=5.0, resolution=0.1, orient=tk.HORIZONTAL, label="Contraste")
@@ -101,47 +105,47 @@ def fenetre_effet(fenetre=""):
         slider_pente.set(10)
         slider_pente.pack(pady=20)
 
+        tk.Button(frame_boutons, text="Aperçu", command=lambda: apercu(slider_contraste.get(),slider_pente.get(), 2)).pack(side=tk.LEFT, padx=10)
         tk.Button(frame_boutons, text="Appliquer", command=lambda: sigmoide(slider_contraste.get(), slider_pente.get())).pack(side=tk.LEFT, padx=10)
         tk.Button(frame_boutons, text="Annuler", command=dialogue_effet.destroy).pack(side=tk.LEFT, padx=10)
 
 def filtre_couleur(filtre=""):
-    global matrice_pixels
+    global image_matrice
     sauvegarder_etat()
-    
+
     if filtre == "vert":
-        for i in range(len(matrice_pixels)):
-            for j in range(len(matrice_pixels[0])):
-                r, g, b = matrice_pixels[i][j]
-                matrice_pixels[i][j] = (0, g, 0)
+        for i in range(len(image_matrice)):
+            for j in range(len(image_matrice[0])):
+                r, g, b = image_matrice[i][j]
+                image_matrice[i][j] = (0, g, 0)
 
     elif filtre == "gris":
-        for y in range(len(matrice_pixels)):
-            for x in range(len(matrice_pixels[0])):
-                r, g, b = matrice_pixels[y][x]
+        for y in range(len(image_matrice)):
+            for x in range(len(image_matrice[0])):
+                r, g, b = image_matrice[y][x]
                 gris = int(0.299 * r + 0.587 * g + 0.114 * b)
-                matrice_pixels[y][x] = (gris, gris, gris)
+                image_matrice[y][x] = (gris, gris, gris)
     refresh()
 
 def correction_gamma(valeur):
-    global matrice_affichage, matrice_pixels
+    global image_matrice
     sauvegarder_etat()
     gamma = float(valeur)
     max_value = 255.0
-    for y in range(len(matrice_affichage)):
-        for x in range(len(matrice_affichage[0])):
-            r, g, b = matrice_affichage[y][x]
-            matrice_affichage[y][x] = (
+    for y in range(len(image_matrice)):
+        for x in range(len(image_matrice[0])):
+            r, g, b = image_matrice[y][x]
+            image_matrice[y][x] = (
                 int(max_value * (r / max_value) ** gamma),
                 int(max_value * (g / max_value) ** gamma),
                 int(max_value * (b / max_value) ** gamma)
             )
-    matrice_pixels = [row[:] for row in matrice_affichage]
     refresh()
     dialogue_effet.destroy()
 
     
 def appliquer_flou_gaussien(intensite):
-    global matrice_pixels
+    global image_matrice
     sauvegarder_etat()
 
     kernel = np.array([
@@ -151,48 +155,55 @@ def appliquer_flou_gaussien(intensite):
     ], dtype=np.float32)
     kernel /= kernel.sum()
 
-    image_np = np.array(matrice_pixels, dtype=np.uint8)
+    image_np = np.array(image_matrice, dtype=np.uint8)
     flou_image = image_np.copy()
 
     for i in range(int(intensite)):
         for j in range(3):
             flou_image[:, :, j] = convolve2d(flou_image[:, :, j], kernel, mode='same', boundary='symm')
 
-    matrice_pixels = []
-    for y in range(flou_image.shape[0]):
-        row = []
-        for x in range(flou_image.shape[1]):
-            row.append(tuple(flou_image[y, x].astype(int)))
-        matrice_pixels.append(row)
+    image_matrice = [[tuple(pixel) for pixel in row] for row in flou_image]
 
     refresh()
     dialogue_effet.destroy()
 
 
 def sigmoide(c, k):
-    global matrice_pixels
+    global image_matrice
 
     sauvegarder_etat()
-    matrice_pixels = [[(
+    image_matrice = [[(
         int(1/(1+np.exp(-k*c*(r/255.0-0.5)))*255),
         int(1/(1+np.exp(-k*c*(g/255.0-0.5)))*255),
         int(1/(1+np.exp(-k*c*(b/255.0-0.5)))*255)
-    ) for (r, g, b) in row] for row in matrice_pixels]
+    ) for (r, g, b) in row] for row in image_matrice]
 
     refresh() 
     dialogue_effet.destroy()
 
+def apercu(param1, param2, filtre):
+    sauvegarder_etat()
+    if filtre==0:
+        correction_gamma(param1)
+    elif filtre==1:
+        appliquer_flou_gaussien(param1)
+    elif filtre==2:
+        sigmoide(param1, param2)
+    refresh()
+    fenetre_principale.after(1000, undo)
+    fenetre_principale.after(1500, lambda:fenetre_effet(filtre))
+
 
 def enregistrer():
-    if not matrice_pixels:
+    if not image_matrice or not image_selectionnee:
         return
 
-    hauteur = len(matrice_pixels)
-    largeur = len(matrice_pixels[0])
+    hauteur = len(image_matrice)
+    largeur = len(image_matrice[0])
     image_finale = Image.new("RGB", (largeur, hauteur))
     for i in range(hauteur):
         for j in range(largeur):
-            image_finale.putpixel((j, i), matrice_pixels[i][j])
+            image_finale.putpixel((j, i), image_matrice[i][j])
 
     fichier = filedialog.asksaveasfilename(defaultextension=".png",
                                         filetypes=[("PNG", "*.png"), ("JPEG", "*.jpg;*.jpeg"), ("BMP", "*.bmp"), ("GIF", "*.gif")],
@@ -203,6 +214,9 @@ def enregistrer():
 
 fenetre_principale = tk.Tk()
 fenetre_principale.title("UVSQolor")
+fenetre_principale.bind('<Control-z>', lambda event: undo())
+fenetre_principale.bind('<Control-s>', lambda event:enregistrer())
+fenetre_principale.bind('<Control-n>', lambda event: ouvrir())
 
 canvas = tk.Canvas(fenetre_principale)
 canvas.pack()
@@ -215,12 +229,23 @@ menu_fichier.add_command(label="Enregistrer", command=enregistrer, state="disabl
 barre_menu.add_cascade(label="Fichier", menu=menu_fichier)
 
 menu_effets = Menu(barre_menu, tearoff=0)
-menu_effets.add_command(label="Filtre vert", command=lambda: filtre_couleur("vert"), state="disabled")
-menu_effets.add_command(label="Niveaux de gris", command=lambda: filtre_couleur("gris"), state="disabled")
-menu_effets.add_command(label="Luminosité", command=lambda: fenetre_effet("luminosité"), state="disabled")
-menu_effets.add_command(label="Contraste", command=lambda: fenetre_effet("contraste"), state="disabled")
-menu_effets.add_command(label="Flou gaussien", command=lambda: fenetre_effet("flou"), state="disabled")
-menu_effets.add_command(label="Annuler", command=undo, state="disabled")
+
+sous_menu_filtres = Menu(menu_effets, tearoff=0)
+sous_menu_filtres.add_command(label="Filtre vert", command=lambda: filtre_couleur("vert"), state="disabled")
+sous_menu_filtres.add_command(label="Niveaux de gris", command=lambda: filtre_couleur("gris"), state="disabled")
+menu_effets.add_cascade(label="Filtres", menu=sous_menu_filtres)
+
+sous_menu_ajustements = Menu(menu_effets, tearoff=0)
+sous_menu_ajustements.add_command(label="Luminosité", command=lambda: fenetre_effet(0), state="disabled")
+sous_menu_ajustements.add_command(label="Contraste", command=lambda: fenetre_effet(2), state="disabled")
+menu_effets.add_cascade(label="Ajustements", menu=sous_menu_ajustements)
+
+sous_menu_flou = Menu(menu_effets, tearoff=0)
+sous_menu_flou.add_command(label="Flou gaussien", command=lambda: fenetre_effet(1), state="disabled")
+menu_effets.add_cascade(label="Flou", menu=sous_menu_flou)
 barre_menu.add_cascade(label="Effets", menu=menu_effets)
+
+barre_menu.add_command(label="↶", command=undo)
+
 
 fenetre_principale.mainloop()
