9f8a13853a211210f08625512ca09065bf2f590b
Anis TOUAT <anis.touat@ens.uvsq.fr>
2025-05-11 19:40:21 +0200

    update

--- Projet UVSQColor.py
+++ Projet UVSQColor.py
@@ -206,7 +206,7 @@ def enregistrer():
             image_finale.putpixel((j, i), image_matrice[i][j])
 
     fichier = filedialog.asksaveasfilename(defaultextension=".png",
-                                        filetypes=[("PNG", "*.png"), ("JPEG", "*.jpg;*.jpeg"), ("BMP", "*.bmp"), ("GIF", "*.gif")],
+                                        filetypes=[("Images", formats)]
                                         title="Enregistrer l'image")
     if fichier:
         image_finale.save(fichier)
