31abfde074368705811c15f9cf24e03b55398d7f
Anis TOUAT <anis.touat@ens.uvsq.fr>
2025-05-16 16:55:20 +0200

    Update

--- Projet UVSQColor.py
+++ Projet UVSQColor.py
@@ -239,35 +239,26 @@ fenetre_principale.title("UVSQolor")
 fenetre_principale.bind('<Control-z>', lambda event: undo())
 fenetre_principale.bind('<Control-s>', lambda event:enregistrer())
 fenetre_principale.bind('<Control-n>', lambda event: ouvrir())
-
-
 barre_menu = Menu(fenetre_principale)
 fenetre_principale.config(menu=barre_menu)
 menu_fichier = Menu(barre_menu, tearoff=0)
 menu_fichier.add_command(label="Ouvrir", command=ouvrir)
 menu_fichier.add_command(label="Enregistrer", command=enregistrer, state="disabled")
 barre_menu.add_cascade(label="Fichier", menu=menu_fichier)
-
 menu_effets = Menu(barre_menu, tearoff=0)
 menu_effets.add_command(label="Fusionner", command=fusionner, state="disabled")
-
 sous_menu_filtres = Menu(menu_effets, tearoff=0)
 sous_menu_filtres.add_command(label="Filtre vert", command=lambda: callback_filtre_couleur("vert"), state="disabled")
 sous_menu_filtres.add_command(label="Niveaux de gris", command=lambda: callback_filtre_couleur("gris"), state="disabled")
 sous_menu_filtres.add_command(label="Détection de bords", command=filtre_detection_bords, state="disabled")
 menu_effets.add_cascade(label="Filtres", menu=sous_menu_filtres)
-
 sous_menu_ajustements = Menu(menu_effets, tearoff=0)
 sous_menu_ajustements.add_command(label="Luminosité", command=lambda: callback_fenetre_effet("luminosité"), state="disabled")
 sous_menu_ajustements.add_command(label="Contraste", command=lambda: callback_fenetre_effet("contraste"), state="disabled")
 menu_effets.add_cascade(label="Ajustements", menu=sous_menu_ajustements)
-
 sous_menu_flou = Menu(menu_effets, tearoff=0)
 sous_menu_flou.add_command(label="Flou gaussien", command=lambda: callback_fenetre_effet("flou"), state="disabled")
 menu_effets.add_cascade(label="Flou", menu=sous_menu_flou)
 barre_menu.add_cascade(label="Effets", menu=menu_effets)
-
 barre_menu.add_command(label="↶", command=undo)
-
-
 fenetre_principale.mainloop()
